3603a1a2a3e9558000472f3aaa08672d7cdbf0d3
Static1014 <420048248@qq.com>
2015-04-08 20:58:11 +0800

    Swift学习
    swift学习记录

---
+++ mSwift.playground/section-1.swift
@@ -0,0 +1,23 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let str1 = "数值转换"
+let height = 1.32
+let int = Int(height)
+let float = Float(height)
+let double = Double(height)
+
+var boolean = true
+var b = Bool(0)
+b = Bool(1)
+
+
+
+
+
+
+
+
