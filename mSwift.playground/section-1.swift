fde8f118ad0d4de89d38d0ab329227657a012388
Static1014 <420048248@qq.com>
2015-04-09 09:21:38 +0800

    4.8

--- mSwift.playground/section-1.swift
+++ mSwift.playground/section-1.swift
@@ -6,6 +6,7 @@ var str = "Hello, playground"
 let 变量 = "!!!!! \n 变量是可以以中文命名的"
 println(变量)
 
+// 数值类型转换
 let str1 = "数值转换"
 let height = 1.32
 let int = Int(height)
@@ -16,7 +17,8 @@ var boolean = true
 var b = Bool(0)
 b = Bool(1)
 
-let 元组 = "元祖 ------ 数组"
+// 元组
+let 元组 = "元组 ------ 数组"
 let man = ("Jack", 32, 1.69)
 
 let (name, age, tall) = man
@@ -33,8 +35,174 @@ let woman = (wName:"女人", wAge:32, wHeight:1.73)
 
 println("\n元组下标命名 \n name : \(woman.wName) \n age : \(woman.wAge) \n tall : \(woman.wHeight)")
 
+// 可选类型
+let 可选类型 = "可选类型"; println("\n\(可选类型)")
+var value:Int?
+if value == nil {
+    println("value is nil")
+} else {
+    println("value is \(value)")
+}
+value = 123
+// 断言，如果表达式为false，中断执行
+assert(value != nil, "断言")
+println("可选类型要用value!的方式取值 value is \(value)")
+println("value is \(value!)")
+
+// 取余数
+println("\n9%5 = \(9%5)")
+println("9%2.5 = \(9%2.5)")
+
+// 区间表达式
+var line = 1...5
+println("\n\(line)")
+line = 1..<5
+
+// ***************    字符串   ***************
+var str11 = ""
+var str12 = String()
+
+if str11.isEmpty {
+    println("\nstr11 is nil")
+}
+
+var st:String?
+if st == nil {
+    st = "st"
+    if st != nil {
+        println("st : \(st!)")
+    }
+}
+
+var msg1 = "11111", msg2 = "11111", msg3 = "1111133311111"
+if msg1 == msg2 {
+    println("msg1 == msg2")
+}
+if msg3.hasPrefix(msg1) {
+    println("以msg1开头 msg3 has prefix msg1")
+}
+if msg3.hasSuffix(msg1) {
+    println("以msg1结尾 msg3 has suffix msg1")
+}
+
+// 数组
+var 空数组 = []
+var empty:[String] = [String]()
+var 漫画书 = ["海贼王", "死神", "进击的巨人"]
+var 混合数组 = [12, "3123", false]
+println(empty)
+println(漫画书)
+混合数组[0] = "oodsafs"
+println(混合数组)
+
+println("\(混合数组[1])+\(混合数组[2])")
+var i = 混合数组[0]
+var j = 混合数组[1]
+var m = 混合数组[2]
+
+var 长数组 = [111, 222, 333, 444, 555]
+长数组[0] = 9
+println(长数组)
+长数组.insert(111, atIndex: 0)
+println(长数组)
+长数组.removeAtIndex(0)
+println(长数组)
+长数组.removeAtIndex(0)
+println(长数组)
+长数组.insert(111, atIndex: 0)
+长数组.insert(000, atIndex: 0)
+println(长数组)
+println(长数组.count)
+for valuee in 长数组 {
+    println("\(valuee)")
+}
+for (index, temp) in enumerate(长数组) {
+    println("\(index) : \(temp)")
+}
+for index in 0...3 {
+    println("--------\(长数组[index])")
+}
+
+// 字典
+var dic:Dictionary? = Dictionary<String, String>()
+var dic1 = Dictionary<Int, String>()
 
 
+dic = ["0":"000", "1":"111"]
+println(dic)
 
+dic1 = [0:"000", 1:"111"]
+println(dic1)
 
+dic1[2] = "222"
+println(dic1)
+dic1[2] = nil
+println(dic1)
+// 遍历字典
+for (key, value) in dic1 {
+    println("\(key) : \(value)")
+}
+for key in dic1.keys {
+    println("\(key) ")
+}
+for value in dic1.values {
+    println("\(value)")
+}
+
+// switch语句
+var n = 4
+switch n {
+case 0...1 :
+    println("0...1")
+case 2...6:
+    println("2...6")
+default:
+    println("default")
+}
+
+var nn = "111"
+switch nn {
+case "1","11","111":
+    println("111111111111")
+case "2","222":
+    println("2222222222222")
+default:
+    println("ddddd")
+}
+var point = (0, 2)
+switch point {
+case (0, 0):
+    println("0,0")
+case (0...999, 0...999):
+    println("111")
+    fallthrough // 穿透
+case (_, 0...2):
+    println("222")
+default:
+    println("default")
+}
+
+// 函数
+
+func 取最小值(参数1:Int, 参数2:Int) -> Int {
+    if 参数1 < 参数2 {
+        return 参数1
+    } else {
+        return 参数2
+    }
+}
+
+println("------ \(取最小值(1, 3))")
+
+func add(var value:Int) {
+    ++value
+}
+println(add(1))
+
+var iii = 11
+func add(inout value:Int) {
+    ++value
+}
+add(&iii)
+println(iii)
 
